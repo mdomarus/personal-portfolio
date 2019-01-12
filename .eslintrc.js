3bcaba0237bcabd7dbba8fda2296deadd28ccf8c
Michał Domarus <mdomarus@gmail.com>
2019-01-12 18:37:30 +0000

    Packages update, setup

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+    "extends": [
+        "eslint:recommended",
+        "airbnb"
+    ],
+    "rules": {
+        "react/jsx-filename-extension": 0,
+    },
+    "env": {
+        "browser": true,
+        "jest": true
+    }
+};
