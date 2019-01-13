dd0417305f1f529686b6bbd52b3c1462db7f7951
Michał Domarus <mdomarus@gmail.com>
2019-01-13 12:27:25 +0000

    Paris

--- src/pages/travel/paris.js
+++ src/pages/travel/paris.js
@@ -1,7 +1,9 @@
 import React from 'react';
+import { graphql } from 'gatsby';
+import Img from 'gatsby-image';
 import Page from '../../components/Page';
 
-export default () => (
+export default ({ data }) => (
   <Page>
     <h1>Paris 2016</h1>
     <p>
@@ -13,5 +15,39 @@ export default () => (
       cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
       est laborum.
     </p>
+    {data.images.edges.map(el => (
+      <Img className="image" fluid={el.node.childImageSharp.fluid} />
+    ))}
   </Page>
 );
+
+export const query = graphql`
+  query {
+    images: allFile(
+      filter: {
+        sourceInstanceName: { eq: "images" }
+        relativePath: { regex: "/paris/" }
+      }
+      sort: { order: ASC, fields: [name] }
+    ) {
+      edges {
+        node {
+          childImageSharp {
+            fluid(maxWidth: 2048) {
+              base64
+              tracedSVG
+              aspectRatio
+              src
+              srcSet
+              srcWebp
+              srcSetWebp
+              sizes
+              originalImg
+              originalName
+            }
+          }
+        }
+      }
+    }
+  }
+`;
