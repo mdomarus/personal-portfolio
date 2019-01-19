2378b83a0876644529e7d3ab4224bbcf07ce1d58
Michał Domarus <mdomarus@gmail.com>
2019-01-19 22:51:48 +0000

    BackToTop icon svg

--- src/components/BackToTop.js
+++ src/components/BackToTop.js
@@ -14,6 +14,13 @@ export default class BackToTop extends React.Component {
       return this.state.isScrollingUp !== nextState.isScrollingUp;
     }
 
+    handleClick = (ev) => {
+      this.setState({
+        scrollPos: -1,
+        isScrollingUp: false,
+      });
+    }
+
     handleScroll = () => {
       this.setState({
         isScrollingUp: this.state.scrollPos > window.scrollY,
@@ -24,7 +31,7 @@ export default class BackToTop extends React.Component {
     render() {
       const classes = this.state.isScrollingUp ? 'backToTop backToTop--active' : 'backToTop';
       return (
-        <a href="#" className={classes}><span role="img" aria-label="back to top">🔝</span></a>
+        <a href="#" className={classes} onClick={this.handleClick}><span role="img" aria-label="back to top"><svg width="24" height="24" className={classes} xmlns="http://www.w3.org/2000/svg" fillRule="evenodd" clipRule="evenodd"><path d="M12 0c6.623 0 12 5.377 12 12s-5.377 12-12 12-12-5.377-12-12 5.377-12 12-12zm0 1c6.071 0 11 4.929 11 11s-4.929 11-11 11-11-4.929-11-11 4.929-11 11-11zm5.247 15l-5.247-6.44-5.263 6.44-.737-.678 6-7.322 6 7.335-.753.665z"/></svg></span></a>
       );
     }
 }
