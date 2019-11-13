9e61dce275bab5327d472bf2850e65ac84cf4dbf
Michał Domarus <mdomarus@gmail.com>
2019-11-13 08:37:10 +0000

    ENV variables

--- functions/hello/hello.js
+++ functions/hello/hello.js
@@ -1,21 +1,24 @@
-// require('dotenv').config();
+require('dotenv').config();
 const sendMail = require('sendmail')();
+
+const { CONTACT_TO_EMAIL_ADDRESS, FROM_EMAIL_ADDRESS } = process.env;
 
 exports.handler = (event, context, callback) => {
   const clientIP = event.headers['client-ip'];
   const url = JSON.parse(event.body);
 
   const descriptor = {
-    from: '"domar.us website" <mdomarus@gmail.com>',
-    to: 'mdomarus@gmail.com',
+    from: `"domar.us website" <${FROM_EMAIL_ADDRESS}>`,
+    to: CONTACT_TO_EMAIL_ADDRESS,
     subject: `A new HighFive for ${url} from ${clientIP}`,
+    html: 'Someone like your photos. Nice :P',
   };
 
   sendMail(descriptor, (e) => {
     if (e) {
       return callback(null, {
         statusCode: 500,
-        body: e.message,
+        body: { message: e.message, descriptor },
       });
     }
     return callback(null, {
