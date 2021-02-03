884e522fabfcbdd3441ff54289d766cf56f3af42
Michał Domarus <mdomarus@gmail.com>
2021-02-03 00:07:04 +0000

    Update package

--- gatsby-michal-domarus-personal/gatsby-config.js
+++ gatsby-michal-domarus-personal/gatsby-config.js
@@ -17,7 +17,7 @@ module.exports = {
       },
     },
     {
-      resolve: 'gatsby-plugin-prefetch-google-fonts',
+      resolve: 'gatsby-plugin-google-fonts',
       options: {
         fonts: [
           {
